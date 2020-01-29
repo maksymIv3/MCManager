adf1f5eb505c527bb6e8108e9feea034fb0e3a1f
Maksym Ivanyk <maksym.ivanyk@rolique.io>
2020-01-29 13:59:14 +0200

    Fix types

--- MCManager/MCObserver.m
+++ MCManager/MCObserver.m
@@ -20,11 +20,11 @@
     return _observers;
 }
 
-- (void)addObserver:(Class<MCObserverProtocol>)object {
+- (void)addObserver:(id<MCObserverProtocol>)object {
     [self.observers addObject:[object class]];
 }
 
-- (void)removeObserver:(Class<MCObserverProtocol>)object {
+- (void)removeObserver:(id<MCObserverProtocol>)object {
     NSUInteger objectIndex = [self.observers indexOfObject:[object class]];
     if (objectIndex) {
         [self.observers removeObjectAtIndex:objectIndex];
@@ -32,41 +32,41 @@
 }
 
 - (void)notifyObserversDidChangeState:(MCSessionState)state peer:(MCPeerID *)peerID {
-    for (NSObject *observer in self.observers) {
+    for (id<MCObserverProtocol> observer in self.observers) {
         if ([observer respondsToSelector:@selector(didChangeState:peer:)]) {
-            [(id<MCObserverProtocol>)observer didChangeState:state peer:peerID];
+            [observer didChangeState:state peer:peerID];
         }
     }
 }
 
 - (void)notifyObserversDidReceiveData:(NSData *)data fromPeer:(MCPeerID *)peerID {
-    for (NSObject *observer in self.observers) {
+    for (id<MCObserverProtocol> observer in self.observers) {
         if ([observer respondsToSelector:@selector(didReceiveData:fromPeer:)]) {
-            [(id<MCObserverProtocol>)observer didReceiveData:data fromPeer:peerID];
+            [observer didReceiveData:data fromPeer:peerID];
         }
     }
 }
 
 - (void)notifyObserversDidStartReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID withProgress:(NSProgress *)progress {
-    for (NSObject *observer in self.observers) {
+    for (id<MCObserverProtocol> observer in self.observers) {
         if ([observer respondsToSelector:@selector(didStartReceivingResourceWithName:fromPeer:withProgress:)]) {
-            [(id<MCObserverProtocol>)observer didStartReceivingResourceWithName:resourceName fromPeer:peerID withProgress:progress];
+            [observer didStartReceivingResourceWithName:resourceName fromPeer:peerID withProgress:progress];
         }
     }
 }
 
 - (void)notifyObserversDidFinishReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID atURL:(NSURL *)localURL withError:(NSError *)error {
-    for (NSObject *observer in self.observers) {
+    for (id<MCObserverProtocol> observer in self.observers) {
         if ([observer respondsToSelector:@selector(didFinishReceivingResourceWithName:fromPeer:atURL:withError:)]) {
-            [(id<MCObserverProtocol>)observer didFinishReceivingResourceWithName:resourceName fromPeer:peerID atURL:localURL withError:error];
+            [observer didFinishReceivingResourceWithName:resourceName fromPeer:peerID atURL:localURL withError:error];
         }
     }
 }
 
 - (void)notifyObserversDidReceiveStream:(NSInputStream *)stream withName:(NSString *)streamName fromPeer:(MCPeerID *)peerID {
-    for (NSObject *observer in self.observers) {
+    for (id<MCObserverProtocol> observer in self.observers) {
         if ([observer respondsToSelector:@selector(didReceiveStream:withName:fromPeer:)]) {
-            [(id<MCObserverProtocol>)observer didReceiveStream:stream withName:streamName fromPeer:peerID];
+            [observer didReceiveStream:stream withName:streamName fromPeer:peerID];
         }
     }
 }
